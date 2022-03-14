1701e5b0458bb045d395caf3a6a8a7ab33fcf6bd
Diego Torres <diegotc86@gmail.com>
2022-03-14 07:09:09 -0500

    Add calculator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import Calculator from "./components/Calculator";
+import { BsFillCartFill } from "react-icons/bs";
+import { colors } from "./styles";
+
+const category = {
+  color: colors.red[500],
+  name: "Groceries",
+  icon: BsFillCartFill,
+};
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Calculator category={category} />
+    </div>
+  );
 }
 
 export default App;
