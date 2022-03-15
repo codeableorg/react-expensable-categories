43569ec18b904312c41590d0465ed6db6c3b1c47
Diego Torres <diegotc86@gmail.com>
2022-03-15 10:50:12 -0500

    Erase App page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,5 @@
-import Calculator from "./components/Calculator";
-import { BsFillCartFill } from "react-icons/bs";
-import { colors } from "./styles";
-
-const category = {
-  color: colors.red[500],
-  name: "Groceries",
-  icon: BsFillCartFill,
-};
-
 function App() {
-  return (
-    <div>
-      <Calculator category={category} />
-    </div>
-  );
+  return <div>Expensable App Coming Soon</div>;
 }
 
 export default App;
