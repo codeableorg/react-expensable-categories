65ea7ee8d763021bbef96ffce226638187056197
Diego Torres <diegotc86@gmail.com>
2022-03-15 18:35:00 -0500

    Refactor styles

--- src/components/SidebarNav/sidebar-nav-item.js
+++ src/components/SidebarNav/sidebar-nav-item.js
@@ -1,8 +1,5 @@
-// /** @jsxImportSource @emotion/react */
-
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
-// import {css} from "@emotion/react"
 import { colors, typography } from "../../styles";
 import { Link } from "react-router-dom";
 
@@ -19,37 +16,38 @@ const currentStyles = (current) => {
 };
 
 const StyledAnchor = styled.div`
-  & > a {
-    padding: 0.5rem;
-    display: flex;
-    gap: 0.75rem;
-    text-decoration: none;
-    ${typography.text.md};
-    color: ${colors.gray[600]};
-    font-weight: 500;
-    align-items: center;
-    border-radius: 0.375rem;
-    cursor: pointer;
-    text-decoration: none;
-    &:hover {
-      background-color: ${colors.pink[100]};
-    }
-    &:focus {
-      outline: 1px solid ${colors.pink[500]};
-    }
-
-    ${(props) => currentStyles(props.current)}
+  padding: 0.5rem;
+  display: flex;
+  gap: 0.75rem;
+  text-decoration: none;
+  ${typography.text.md};
+  color: ${colors.gray[600]};
+  font-weight: 500;
+  align-items: center;
+  border-radius: 0.375rem;
+  cursor: pointer;
+  text-decoration: none;
+  &:hover {
+    background-color: ${colors.pink[100]};
   }
+  &:focus {
+    outline: 1px solid ${colors.pink[500]};
+  }
+  &:visited {
+    color: ${colors.gray[600]};
+  }
+
+  ${(props) => currentStyles(props.current)}
 `;
 
-function SidebarNavItem({ name, icon, to, ...rest }) {
+function SidebarNavItem({ name, icon, to, current }) {
   return (
-    <StyledAnchor {...rest}>
-      <Link to={to}>
+    <Link to={to}>
+      <StyledAnchor current={current}>
         {icon}
         {name}
-      </Link>
-    </StyledAnchor>
+      </StyledAnchor>
+    </Link>
   );
 }
 
